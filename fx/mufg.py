1454e9bd1d60680a1c138412049373724cc56bbb
Ian Lewis <ian@ianlewis.org>
2025-03-11 02:15:11 +0000

    feat: Add retries and timeouts
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

--- fx/mufg.py
+++ fx/mufg.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from urllib import request
+import urllib3
 
 from bs4 import BeautifulSoup
 
@@ -89,69 +89,74 @@ class MUFGProvider:
 
         url = f'https://murc-kawasesouba.jp/fx/past_3month_result.php?y={quote_date.strftime("%Y")}&m={quote_date.strftime("%m")}&d={quote_date.strftime("%d")}'
         self.logger.debug(f"GET: {url}")
-        resp = request.urlopen(url)
+
+        http = urllib3.PoolManager(
+            retries=urllib3.Retry(connect=5, read=5, redirect=2),
+            timeout=urllib3.Timeout(connect=2.0, read=7.0),
+        )
+
+        resp = http.request("GET", url)
 
         quotes = []
-        with resp as f:
-            body = f.read()
-            try:
-                # NOTE: Some pages say they are EUC-JP but they are actually SHIFT-JIS.
-                body = body.decode("shift-jis")
-            except UnicodeDecodeError as e:
-                # The decoding error is logged at debug level since it's "expected".
-                self.logger.debug("%s %s", quote_date, e)
+        body = resp.data
+        try:
+            # NOTE: Some pages say they are EUC-JP but they are actually SHIFT-JIS.
+            body = body.decode("shift-jis")
+        except UnicodeDecodeError as e:
+            # The decoding error is logged at debug level since it's "expected".
+            self.logger.debug("%s %s", quote_date, e)
 
-            soup = BeautifulSoup(body, "html.parser")
-            table = soup.select_one("table.data-table5")
+        soup = BeautifulSoup(body, "html.parser")
+        table = soup.select_one("table.data-table5")
 
-            if not table:
-                # This most likely means that there are no quotes for this day.
-                return quotes
+        if not table:
+            # This most likely means that there are no quotes for this day.
+            return quotes
 
-            # NOTE: The HTML on this page is invalid. It contains </tr> but no
-            #       opening <tr> tag for table rows after the first row. Thus we
-            #       don't select for <tr> tags but go directly to the <td> tag.
-            kwargs = {
-                "provider_code": self.code,
-                "date": Date(
-                    year=quote_date.year,
-                    month=quote_date.month,
-                    day=quote_date.day,
-                ),
-                "quote_currency_code": self.currencies["JPY"].alphabetic_code,
-            }
+        # NOTE: The HTML on this page is invalid. It contains </tr> but no
+        #       opening <tr> tag for table rows after the first row. Thus we
+        #       don't select for <tr> tags but go directly to the <td> tag.
+        kwargs = {
+            "provider_code": self.code,
+            "date": Date(
+                year=quote_date.year,
+                month=quote_date.month,
+                day=quote_date.day,
+            ),
+            "quote_currency_code": self.currencies["JPY"].alphabetic_code,
+        }
 
-            for i, cell in enumerate(table.select("td")):
-                match i % 6:
-                    case 0:
-                        # Base currency name is ignored.
+        for i, cell in enumerate(table.select("td")):
+            match i % 6:
+                case 0:
+                    # Base currency name is ignored.
+                    pass
+                case 1:
+                    # Japanese name is ignored
+                    pass
+                case 2:
+                    kwargs["base_currency_code"] = self.currencies[cell.get_text(strip=True)].alphabetic_code
+                case 3:
+
+                    try:
+                        kwargs["ask"] = str_to_money(jpy_code, cell.get_text(strip=True))
+                    except ValueError as e:
+                        self.logger.debug(f"tts: {type(e).__name__}: {e}")
                         pass
-                    case 1:
-                        # Japanese name is ignored
+                case 4:
+                    try:
+                        kwargs["bid"] = str_to_money(jpy_code, cell.get_text(strip=True))
+                    except ValueError as e:
+                        self.logger.debug(f"ttb: {type(e).__name__}: {e}")
                         pass
-                    case 2:
-                        kwargs["base_currency_code"] = self.currencies[cell.get_text(strip=True)].alphabetic_code
-                    case 3:
+                case 5:
+                    try:
+                        kwargs["mid"] = str_to_money(jpy_code, cell.get_text(strip=True))
+                    except ValueError as e:
+                        self.logger.debug(f"ttm: {type(e).__name__}: {e}")
+                        pass
 
-                        try:
-                            kwargs["ask"] = str_to_money(jpy_code, cell.get_text(strip=True))
-                        except ValueError as e:
-                            self.logger.debug(f"tts: {type(e).__name__}: {e}")
-                            pass
-                    case 4:
-                        try:
-                            kwargs["bid"] = str_to_money(jpy_code, cell.get_text(strip=True))
-                        except ValueError as e:
-                            self.logger.debug(f"ttb: {type(e).__name__}: {e}")
-                            pass
-                    case 5:
-                        try:
-                            kwargs["mid"] = str_to_money(jpy_code, cell.get_text(strip=True))
-                        except ValueError as e:
-                            self.logger.debug(f"ttm: {type(e).__name__}: {e}")
-                            pass
-
-                        if kwargs.get("ask") or kwargs.get("bid") or kwargs.get("mid"):
-                            quotes.append(Quote(**kwargs))
+                    if kwargs.get("ask") or kwargs.get("bid") or kwargs.get("mid"):
+                        quotes.append(Quote(**kwargs))
 
         return quotes
