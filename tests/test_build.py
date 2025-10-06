3d36a285b35c9eafbf6df91c63002a0b6e703e0d
Ian Lewis <ian@ianlewis.org>
2025-10-06 07:15:46 +0000

    test: add test for the build command
    Signed-off-by: Ian Lewis <ian@ianlewis.org>

---
+++ tests/test_build.py
@@ -0,0 +1,199 @@
+# Copyright 2025 Ian Lewis
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for the build command."""
+
+import argparse
+import logging
+import tempfile
+import unittest
+from pathlib import Path
+
+from fx.build import build_command
+from fx.currency import CurrencyList
+from fx.quote_pb2 import QuoteList
+from tests.mock_provider import MockProvider
+
+
+def _write_test_data(data_dir_path: Path) -> None:
+    """Write test data."""
+    clist = CurrencyList()
+    currency = clist.currencies.add()
+    currency.alphabetic_code = "USD"
+    currency.numeric_code = "840"
+    currency.name = "US Dollar"
+    currency.minor_units = 2
+    currency.countries.extend(["UNITED STATES OF AMERICA (THE)"])
+
+    currency_file_path = data_dir_path.joinpath("currencies.binpb")
+    currency_file_path.parent.mkdir(parents=True, exist_ok=True)
+    with currency_file_path.open("wb") as f:
+        f.write(clist.SerializeToString())
+
+    quotes = QuoteList()
+    quote = quotes.quotes.add()
+    quote.provider_code = "MOCK"
+    quote.base_currency_code = "USD"
+    quote.quote_currency_code = "JPY"
+    quote.date.year = 2023
+    quote.date.month = 1
+    quote.date.day = 1
+    quote.ask.units = 131
+    quote.ask.nanos = 500000000
+    quote.bid.units = 129
+    quote.bid.nanos = 500000000
+    quote.mid.units = 130
+    quote.mid.nanos = 0
+
+    quote = quotes.quotes.add()
+    quote.provider_code = "MOCK"
+    quote.base_currency_code = "USD"
+    quote.quote_currency_code = "JPY"
+    quote.date.year = 2023
+    quote.date.month = 1
+    quote.date.day = 2
+    quote.ask.units = 141
+    quote.ask.nanos = 500000000
+    quote.bid.units = 139
+    quote.bid.nanos = 500000000
+    quote.mid.units = 140
+    quote.mid.nanos = 0
+
+    quote_file_path = data_dir_path.joinpath("MOCK", "USD", "JPY", "2023.binpb")
+    quote_file_path.parent.mkdir(parents=True, exist_ok=True)
+    with quote_file_path.open("wb") as f:
+        f.write(quotes.SerializeToString())
+
+
+class TestBuildCommand(unittest.TestCase):
+    """Tests for the build command."""
+
+    def setUp(self) -> None:
+        """Set up the test case."""
+        self.temp_data_dir = tempfile.TemporaryDirectory()
+        self.temp_site_dir = tempfile.TemporaryDirectory()
+        _write_test_data(Path(self.temp_data_dir.name))
+
+    def tearDown(self) -> None:
+        """Tear down the test case."""
+        self.temp_data_dir.cleanup()
+        self.temp_site_dir.cleanup()
+
+    def test_build_command(self) -> None:
+        """Test the build command."""
+        build_command(
+            args=argparse.Namespace(
+                logger=logging.getLogger("TestBuildCommand"),
+                data_dir=self.temp_data_dir.name,
+                site_dir=self.temp_site_dir.name,
+                provider=[MockProvider],
+            ),
+        )
+
+        formats = ["json", "csv", "binpb"]
+
+        # Provider
+        for ext in formats:
+            self.assertTrue(
+                Path(self.temp_site_dir.name)
+                .joinpath(
+                    "v1",
+                    "provider",
+                    f"MOCK.{ext}",
+                )
+                .exists(),
+            )
+
+        # latest Quotes
+        for ext in formats:
+            self.assertTrue(
+                Path(self.temp_site_dir.name)
+                .joinpath(
+                    "v1",
+                    "provider",
+                    "MOCK",
+                    "quote",
+                    "USD",
+                    "JPY",
+                    f"latest.{ext}",
+                )
+                .exists(),
+            )
+
+        # Historical Quotes for 2023
+        for ext in formats:
+            self.assertTrue(
+                Path(self.temp_site_dir.name)
+                .joinpath(
+                    "v1",
+                    "provider",
+                    "MOCK",
+                    "quote",
+                    "USD",
+                    "JPY",
+                    f"2023.{ext}",
+                )
+                .exists(),
+            )
+
+        # Monthly Quotes for 2023-01
+        for ext in formats:
+            self.assertTrue(
+                Path(self.temp_site_dir.name)
+                .joinpath(
+                    "v1",
+                    "provider",
+                    "MOCK",
+                    "quote",
+                    "USD",
+                    "JPY",
+                    "2023",
+                    f"01.{ext}",
+                )
+                .exists(),
+            )
+
+        # Daily Quotes for 2023-01-01
+        for ext in formats:
+            self.assertTrue(
+                Path(self.temp_site_dir.name)
+                .joinpath(
+                    "v1",
+                    "provider",
+                    "MOCK",
+                    "quote",
+                    "USD",
+                    "JPY",
+                    "2023",
+                    "01",
+                    f"01.{ext}",
+                )
+                .exists(),
+            )
+
+            self.assertTrue(
+                Path(self.temp_site_dir.name)
+                .joinpath(
+                    "v1",
+                    "provider",
+                    "MOCK",
+                    "quote",
+                    "USD",
+                    "JPY",
+                    "2023",
+                    "01",
+                    f"02.{ext}",
+                )
+                .exists(),
+            )
--- tests/test_update.py
+++ tests/test_update.py
@@ -14,78 +14,17 @@
 
 """Tests for the update command."""
 
+import argparse
 import datetime
 import logging
 import tempfile
 import unittest
 from pathlib import Path
-from typing import Any, ClassVar
 
 import urllib3
-from google.type.date_pb2 import Date
-from google.type.money_pb2 import Money
 
-from fx.quote_pb2 import Quote
 from fx.update import update_command
-
-
-class MockProvider:
-    """A mock provider for testing."""
-
-    code = "MOCK"
-    name = "Mock Provider"
-
-    supported_base_currencies: ClassVar[list[str]] = ["USD", "EUR"]
-    supported_quote_currencies: ClassVar[list[str]] = ["JPY"]
-
-    def __init__(self, args: Any) -> None:  # noqa: ANN401
-        """Initialize the mock provider."""
-
-    def get_quote(
-        self,
-        base_currency_code: str,
-        quote_currency_code: str,
-        quote_date: datetime.date,
-    ) -> Quote | None:
-        """Get a mock quote."""
-        if base_currency_code not in self.supported_base_currencies:
-            return None
-        if quote_currency_code not in self.supported_quote_currencies:
-            return None
-        quote = Quote()
-        quote.provider_code = self.code
-        quote.base_currency_code = base_currency_code
-        quote.quote_currency_code = quote_currency_code
-        quote.date.CopyFrom(
-            Date(
-                year=quote_date.year,
-                month=quote_date.month,
-                day=quote_date.day,
-            ),
-        )
-        quote.bid.CopyFrom(
-            Money(
-                currency_code=quote_currency_code,
-                units=110,
-                nanos=250000000,
-            ),
-        )
-        quote.mid.CopyFrom(
-            Money(
-                currency_code=quote_currency_code,
-                units=111,
-                nanos=250000000,
-            ),
-        )
-        quote.ask.CopyFrom(
-            Money(
-                currency_code=quote_currency_code,
-                units=112,
-                nanos=250000000,
-            ),
-        )
-
-        return quote
+from tests.mock_provider import MockProvider
 
 
 class TestUpdateCommand(unittest.TestCase):
@@ -102,20 +41,6 @@ class TestUpdateCommand(unittest.TestCase):
     @unittest.mock.patch("urllib3.PoolManager")
     def test_update_command(self, mock_pool_manager: urllib3.PoolManager) -> None:
         """Test the update_command function."""
-
-        class Args:
-            logger = logging.getLogger("TestUpdateCommand")
-            data_dir = self.temp_dir.name
-            start = datetime.date(2024, 1, 1)
-            end = datetime.date(2024, 1, 1)
-            provider: ClassVar[list[any]] = [MockProvider]
-            timeout = 10
-            retry = 0
-            backoff = 0
-
-        args = Args()
-        args.logger.addHandler(logging.NullHandler())
-
         mock_currencies_response = unittest.mock.Mock()
         mock_currencies_response.status = 200
         mock_currencies_response.data = (
@@ -154,7 +79,21 @@ class TestUpdateCommand(unittest.TestCase):
             mock_historic_response,
         ]
 
-        update_command(args)
+        logger = logging.getLogger("TestUpdateCommand")
+        logger.addHandler(logging.NullHandler())
+
+        update_command(
+            argparse.Namespace(
+                logger=logger,
+                data_dir=self.temp_dir.name,
+                start=datetime.date(2024, 1, 1),
+                end=datetime.date(2024, 1, 1),
+                provider=[MockProvider],
+                timeout=10,
+                retry=0,
+                backoff=0,
+            ),
+        )
 
         self.assertTrue(Path(self.temp_dir.name).joinpath("currencies.binpb").is_file())
 
